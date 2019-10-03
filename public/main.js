91052d54dde675d7d60f7f98e54786a68eab629d
Tom Galligan <tomgalligan@Toms-MacBook-Pro.local>
2019-10-03 18:06:34 +0100

    add edit item functionality

--- public/main.js
+++ public/main.js
@@ -1,63 +1,104 @@
 window.addEventListener("load", () => {
-    let xhr = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("readystatechange", () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let outputToDo = JSON.parse(xhr.responseText);
+	xhr.addEventListener("readystatechange", () => {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			let outputToDo = JSON.parse(xhr.responseText);
 
-            console.log(outputToDo);
+			console.log(outputToDo);
 
-            for (let i = 0; i < outputToDo.length; i++) {
-                addItemToList(outputToDo[i]);
-            }
-        }
-    });
+			for (let i = 0; i < outputToDo.length; i++) {
+				addItemToList(outputToDo[i]);
+			}
+		}
+	});
 
-    xhr.open("GET", "/items?sortBy=none", true);
-    xhr.send();
+	xhr.open("GET", "/items?sortBy=none", true);
+	xhr.send();
 });
 
 const addItemToList = toDoObj => {
-    const title = toDoObj.title;
-    const para = document.createElement("p");
-    const node = document.createTextNode(title);
-    para.appendChild(node);
-    const checkbox = document.createElement("input");
-    const id = toDoObj.id;
-    const idString = id.toString();
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("id", idString);
-    checkbox.checked = toDoObj.status;
-    para.appendChild(checkbox);
-    document.getElementById("showToDoContainer").appendChild(para);
-    addCheckBoxListener(id);
-    // para.classList.add("para=class");
+	const title = toDoObj.title;
+	const para = document.createElement("p");
+	const node = document.createTextNode(title);
+	para.appendChild(node);
+	const checkbox = document.createElement("input");
+	const id = toDoObj.id;
+	const idString = id.toString();
+	checkbox.setAttribute("type", "checkbox");
+	checkbox.setAttribute("id", idString);
+	checkbox.checked = toDoObj.status;
+	para.appendChild(checkbox);
+	document.getElementById("showToDoContainer").appendChild(para);
+	addCheckBoxListener(id);
+	editButton = document.createElement("button");
+	editButton.setAttribute("id", idString + "-edit");
+	editButton.classList.add("edit-item-button");
+	editIcon = document.createElement("i");
+	editIcon.classList.add("fa", "fa-pencil");
+	editButton.appendChild(editIcon);
+	para.appendChild(editButton);
+	addEditItemListener(toDoObj);
+	// para.classList.add("para=class");
+};
+
+const addEditItemListener = toDoObject => {
+	button = document.getElementById(toDoObject.id.toString() + "-edit");
+	button.addEventListener("click", () => {
+		overlayOn();
+		createEditBox(toDoObject);
+	});
+};
+
+const overlayOn = () => {
+	overlay = document.querySelector(".overlay");
+	overlay.style.display = "block";
+};
+
+const createEditBox = toDoObject => {
+	const overlay = document.querySelector(".overlay");
+	const formNode = document.createElement("form");
+	formNode.class = "form-inline";
+	const inputNode = document.createElement("input");
+	inputNode.type = "text";
+	inputNode.classList.add("edit-form");
+	const editSubmitButton = document.createElement("button");
+	editSubmitButton.setAttribute("id", "edit-submit-button");
+	editSubmitButton.classList.add("submit-edited-item-button");
+	editSubmitButtonText = document.createTextNode("Submit");
+	editSubmitButton.appendChild(editSubmitButtonText);
+	formNode.appendChild(editSubmitButton);
+	formNode.appendChild(inputNode);
+	overlay.appendChild(formNode);
+	inputNode.value = toDoObject.title;
+	document.getElementById("edit-submit-button").addEventListener("click", event => {
+		event.preventDefault();
+		const xhrEditTitle = new XMLHttpRequest();
+		xhrEditTitle.open("PATCH", `items/${toDoObject.id}`, true);
+		xhrEditTitle.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		const body = `title=${inputNode.value}`;
+		xhrEditTitle.send(body);
+	});
 };
 
 const addCheckBoxListener = id => {
-    const checkbox = document.getElementById(id.toString());
-    checkbox.addEventListener("click", () => {
-        console.log("checkbox.checked 1: ", checkbox.checked);
-        let xhrEditStatus = new XMLHttpRequest();
-        xhrEditStatus.addEventListener("readystatechange", () => {
-            if (
-                xhrEditStatus.readyState === 4 &&
-                xhrEditStatus.status === 201
-            ) {
-                // checkbox.checked = !checkbox.checked;
-                console.log("checkbox.value 2: ", checkbox.checked);
-            }
-        });
-        console.log(id, typeof id);
-        xhrEditStatus.open("PATCH", `items/${id}`, true);
-        xhrEditStatus.setRequestHeader(
-            "Content-Type",
-            "application/x-www-form-urlencoded"
-        );
-        console.log("checkbox.checked 3: ", checkbox.checked);
-        const body = `status=${checkbox.checked}`;
-        console.log(body);
-        xhrEditStatus.send(body);
-        console.log("end:", checkbox.checked);
-    });
+	const checkbox = document.getElementById(id.toString());
+	checkbox.addEventListener("click", () => {
+		console.log("checkbox.checked 1: ", checkbox.checked);
+		let xhrEditStatus = new XMLHttpRequest();
+		xhrEditStatus.addEventListener("readystatechange", () => {
+			if (xhrEditStatus.readyState === 4 && xhrEditStatus.status === 201) {
+				// checkbox.checked = !checkbox.checked;
+				console.log("checkbox.value 2: ", checkbox.checked);
+			}
+		});
+		console.log(id, typeof id);
+		xhrEditStatus.open("PATCH", `items/${id}`, true);
+		xhrEditStatus.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		console.log("checkbox.checked 3: ", checkbox.checked);
+		const body = `status=${checkbox.checked}`;
+		console.log(body);
+		xhrEditStatus.send(body);
+		console.log("end:", checkbox.checked);
+	});
 };
--- src/handlers/editItem.js
+++ src/handlers/editItem.js
@@ -7,50 +7,41 @@ const logic = require("../logic.js");
 express().use(formidable());
 
 const editItem = (req, res) => {
-    const toDoFilePath = path.join(__dirname, "..", "..", "/data/todo.json");
-    fs.readFile(toDoFilePath, (error, file) => {
-        if (error) {
-            console.error(error.message);
-            res.status(404).end();
-        } else {
-            const oldToDoListJS = JSON.parse(file);
+	const toDoFilePath = path.join(__dirname, "..", "..", "/data/todo.json");
+	fs.readFile(toDoFilePath, (error, file) => {
+		if (error) {
+			console.error(error.message);
+			res.status(404).end();
+		} else {
+			const oldToDoListJS = JSON.parse(file);
 
-            const itemToEditId = Number(req.params.id);
-            if (!logic.validID(oldToDoListJS, itemToEditId)) {
-                console.error("Invalid item ID supplied");
-                res.status(404).end();
-                return;
-            }
+			const itemToEditId = Number(req.params.id);
+			if (!logic.validID(oldToDoListJS, itemToEditId)) {
+				console.error("Invalid item ID supplied");
+				res.status(404).end();
+				return;
+			}
 
-            const itemToEdit = oldToDoListJS.find(
-                item => item.id === itemToEditId
-            );
+			const itemToEdit = oldToDoListJS.find(item => item.id === itemToEditId);
 
-            const itemToEditIndex = oldToDoListJS.findIndex(
-                item => item === itemToEdit
-            );
+			const itemToEditIndex = oldToDoListJS.findIndex(item => item === itemToEdit);
 
-            const dateNow = Date.now();
-            newToDoListJS = [...oldToDoListJS];
-            console.log("req.fields: ", req.fields);
-            newToDoListJS[itemToEditIndex] = logic.editItemOnList(
-                req.fields,
-                itemToEdit,
-                dateNow
-            );
-            console.log(newToDoListJS);
-
-            const newToDoListJSON = JSON.stringify(newToDoListJS);
-            fs.writeFile(toDoFilePath, newToDoListJSON, error => {
-                if (error) {
-                    console.error(error.message);
-                    res.status(404).end();
-                } else {
-                    res.status(201).end();
-                }
-            });
-        }
-    });
+			const dateNow = Date.now();
+			newToDoListJS = [...oldToDoListJS];
+			console.log("req.fields: ", req.fields);
+			newToDoListJS[itemToEditIndex] = logic.editItemOnList(req.fields, itemToEdit, dateNow);
+			const newToDoListJSON = JSON.stringify(newToDoListJS);
+			fs.writeFile(toDoFilePath, newToDoListJSON, error => {
+				if (error) {
+					console.error(error.message);
+					res.status(404).end();
+				} else {
+					res.status(201).end();
+				}
+				res.redirect(302, "/");
+			});
+		}
+	});
 };
 
 module.exports = editItem;
