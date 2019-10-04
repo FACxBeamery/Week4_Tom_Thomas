0abf00505a7526363b1fcefc60c15e5f20fde34a
Tom Galligan <tomgalligan@Toms-MacBook-Pro.local>
2019-10-04 14:04:43 +0100

    comment out old port setting

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const formidable = require("express-formidable");
 const router = require("./router.js");
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+//const port = process.env.PORT || 3000;
+const port = 5500;
 app.use(formidable());
 
 app.use(router);
